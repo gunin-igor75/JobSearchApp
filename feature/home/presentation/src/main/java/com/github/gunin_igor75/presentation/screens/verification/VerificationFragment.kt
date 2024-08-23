52dabb0e869602de5672023f9835c69678265d0c
Igor Gunin <gunin-igor75@yandex.ru>
2024-08-23 21:13:37 +0600

    #main Реализация fragment_verification.xml

---
+++ feature/home/presentation/src/main/java/com/github/gunin_igor75/presentation/screens/verification/VerificationFragment.kt
@@ -0,0 +1,10 @@
+package com.github.gunin_igor75.presentation.screens.verification
+
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.github.gunin_igor75.common.base.base.BaseFragment
+import com.github.gunin_igor75.presentation.R
+import com.github.gunin_igor75.presentation.databinding.FragmentVerificationBinding
+
+class VerificationFragment : BaseFragment(R.layout.fragment_verification) {
+    private val binding: FragmentVerificationBinding by  viewBinding(R.id.verificationFragment)
+}
