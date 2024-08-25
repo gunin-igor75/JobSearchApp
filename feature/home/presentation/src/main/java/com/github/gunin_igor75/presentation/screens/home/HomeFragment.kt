53931aee59753b38873c99b38728557b82b63561
Igor Gunin <gunin-igor75@yandex.ru>
2024-08-25 11:10:22 +0600

    #main Реализация  fragment_home.xml

---
+++ feature/home/presentation/src/main/java/com/github/gunin_igor75/presentation/screens/home/HomeFragment.kt
@@ -0,0 +1,14 @@
+package com.github.gunin_igor75.presentation.screens.home
+
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.github.gunin_igor75.common.base.base.BaseFragment
+import com.github.gunin_igor75.presentation.R
+import com.github.gunin_igor75.presentation.databinding.FragmentHomeBinding
+
+
+class HomeFragment: BaseFragment(R.layout.fragment_home) {
+
+    private val binding: FragmentHomeBinding by viewBinding(R.id.homeFragment)
+
+
+}
