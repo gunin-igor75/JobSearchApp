758fabf420fc59e8b1df207bc341d403a3d7846b
Igor Gunin <gunin-igor75@yandex.ru>
2024-08-25 14:16:09 +0600

    #main Реализация fragment_vacancies

---
+++ feature/home/presentation/src/main/java/com/github/gunin_igor75/presentation/screens/vacancies/VacanciesFragment.kt
@@ -0,0 +1,11 @@
+package com.github.gunin_igor75.presentation.screens.vacancies
+
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.github.gunin_igor75.common.base.base.BaseFragment
+import com.github.gunin_igor75.presentation.R
+import com.github.gunin_igor75.presentation.databinding.FragmentVacanciesBinding
+
+class VacanciesFragment : BaseFragment(R.layout.fragment_vacancies) {
+    private val binding: FragmentVacanciesBinding by viewBinding(R.id.vacanciesFragment)
+
+}
